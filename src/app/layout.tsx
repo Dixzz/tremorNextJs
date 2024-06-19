6641d4b0ed482d255c4a75ca0138320d51302780
Satyam Sharma <vishusatyam36@gmail.com>
2024-06-19 17:18:52 +0530

    p1

---
+++ src/app/layout.tsx
@@ -0,0 +1,69 @@
+import type { Metadata } from "next"
+import { ThemeProvider } from "next-themes"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+})
+
+import { Sidebar } from "@/components/ui/navigation/sidebar"
+import { siteConfig } from "./siteConfig"
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://yoururl.com"),
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: [],
+  authors: [
+    {
+      name: "yourname",
+      url: "",
+    },
+  ],
+  creator: "yourname",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Tremor OSS Dashboard",
+    creator: '@tremorlabs',
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+    <head>
+      <script src="http://localhost:8097"></script>
+    </head>
+    <body
+      className={`${inter.className} overflow-y-scroll scroll-auto antialiased selection:bg-indigo-100 selection:text-indigo-700`}
+      // className={`${inter.className} overflow-y-scroll scroll-auto antialiased selection:bg-indigo-100 selection:text-indigo-700 dark:bg-gray-950`}
+        suppressHydrationWarning
+      >
+        <div className="mx-auto max-w-screen-2xl">
+          <ThemeProvider defaultTheme="system" attribute="class">
+            <Sidebar />
+            <main className="lg:pl-72">{children}</main>
+          </ThemeProvider>
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/siteConfig.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Dashboard",
+  baseLinks: {
+    home: "/",
+    overview: "/overview",
+    details: "/details",
+    settings: "/settings",
+  },
+}
+
+export type siteConfig = typeof siteConfig
